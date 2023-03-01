de0751246d133b7d84bb2285ff6ae7c43e0b18ba
Ilian Martinez <ili03maro@gmail.com>
2023-03-01 08:37:09 -0600

    adding structure

---
+++ src/main/java/Tiendita.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author ili
+ */
+public class Tiendita {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
